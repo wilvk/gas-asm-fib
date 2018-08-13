8c2ac2d9fb4e20fedcba95575525f3a4e2c2e62a
Willem van Ketwich <willvk@gmail.com>
2018-08-13 17:55:13 +1000

    comment fib2

--- fib2.s
+++ fib2.s
@@ -1,16 +1,15 @@
-# framework
+# stack args example
 .section .text
 .globl _start
   _start:
     nop
-    movl %esp, %ebp
-    addl $8, %ebp # address of first arg in stack
-    movl (%ebp), %ecx
-
-    movl $4, %edx
-    movl $4, %eax
-    movl $0, %ebx
-    int $0x80
-    movl $1, %eax
-    movl $0, %ebx
-    int $0x80
+    movl %esp, %ebp     # take a copy of the stack pointer esp into ebp
+    addl $8, %ebp       # address of first arg in stack
+    movl (%ebp), %ecx   # move the address of the first arg into ecx
+    movl $4, %edx       # set the length of our string to 4
+    movl $4, %eax       # indicate to int 0x80 that we are doing a write
+    movl $0, %ebx       # indicate to int 0x80 that we are writing to file descriptor 0
+    int $0x80           # call int 0x80 for write
+    movl $1, %eax       # exit gracefully
+    movl $0, %ebx       # with return code 0
+    int $0x80           # call int 0x80 for exit
