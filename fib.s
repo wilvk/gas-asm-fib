b443d5a8bc36223dded2ec4c4393ac5c56afe119
Willem van Ketwich <willvk@gmail.com>
2018-08-28 22:01:24 +1000

    remove fib, modify fib1

--- fib.s
+++
@@ -1,8 +0,0 @@
-# framework
-.section .text
-.globl _start
-  _start:
-    nop
-    movl $1, %eax
-    movl $0, %ebx
-    int $0x80
--- fib1.s
+++ fib1.s
@@ -3,6 +3,7 @@
 .globl _start
   _start:
     nop
+    # the rest of our program goes here
     movl $1, %eax
     movl $0, %ebx
     int $0x80
